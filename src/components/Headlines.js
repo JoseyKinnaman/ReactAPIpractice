195455a3a4dd44bb848c8d3b0ad570388179361b
Jozy Kinnaman <jozypants@gmail.com>
2020-07-19 16:37:18 -0700

    add redux and thunk.

--- src/components/Headlines.js
+++ src/components/Headlines.js
@@ -1,43 +1,22 @@
-import React from 'react'
+import React from 'react';
+// We need to import connect from React Redux.
+import { connect } from 'react-redux';
+// We also need to import our ApiCall as well.
+import { makeApiCall } from '../actions';
 
 class Headlines extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      headlines: [],
-    };
-    console.log(props)
-  }
 
-  makeApiCall = () => {
-    fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
-      .then(response => response.json())
-      .then(
-        (jsonifiedResponse) => {
-          this.setState({
-            isLoaded: true,
-            headlines: jsonifiedResponse.results
-          });
-        })
-      .catch((error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      });
-  }
 
   componentDidMount() {
-    this.makeApiCall()
+    const { dispatch } = this.props;
+    dispatch(makeApiCall());
   }
 
   render() {
-    const { error, isLoaded, headlines } = this.state;
+    const { error, isLoading, headlines } = this.props;
     if (error) {
       return <React.Fragment>Error: {error.message}</React.Fragment>;
-    } else if (!isLoaded) {
+    } else if (isLoading) {
       return <React.Fragment>Loading...</React.Fragment>;
     } else {
       return (
@@ -57,4 +36,13 @@ class Headlines extends React.Component {
   }
 }
 
-export default Headlines;
+
+const mapStateToProps = state => {
+  return {
+    headlines: state.headlines,
+    isLoading: state.isLoading,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps)(Headlines);
