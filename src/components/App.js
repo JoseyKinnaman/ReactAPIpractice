6830ddb69bd75d70405fe2041bac7438daf5bf2e
Jozy Kinnaman <jozypants@gmail.com>
2020-07-19 14:03:00 -0700

    set up basic api call.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Headlines from './Headlines'
+
+
+function App() {
+  return (
+  <React.Fragment>
+    <Headlines />
+  </React.Fragment>
+  );
+}
+
+export default App;
